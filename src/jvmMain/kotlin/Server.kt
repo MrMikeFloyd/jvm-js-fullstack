9206500941af08d9183a7c6df88dc35ce50c5e09
Maik <maik@fleuter.xyz>
2020-04-27 13:51:16 +0200

    Add ktor features (content negotiation, CORS etc.)

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,12 +1,33 @@
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CORS
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.gzip
+import io.ktor.http.HttpMethod
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import io.ktor.serialization.json
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
     embeddedServer(Netty, 9090) {
+        // Add & Configure Content Negotiation, CORS and Compression
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            gzip()
+        }
+        // Set up endpoints
         routing {
             get("/hello") {
                 call.respondText { "Hey there, backend here!" }
