9bbb736db7cb2877f718988de21c56bf94b70509
Maik <maik@fleuter.xyz>
2020-04-28 15:02:17 +0200

    Add Text box for adding items and delete function

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,8 +1,11 @@
+import kotlinext.js.jsObject
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.html.js.onClickFunction
 import react.*
-import react.dom.*
-import kotlinext.js.*
-import kotlinx.html.js.*
-import kotlinx.coroutines.*
+import react.dom.h1
+import react.dom.li
+import react.dom.ul
 
 val scope = MainScope()
 
@@ -18,12 +21,35 @@ val App = functionalComponent<RProps> { _ ->
     h1 {
         +"Full-Stack Shopping List"
     }
+    // Shopping list
     ul {
         shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
             li {
-                key = item.toString()
+                // Clicking an item will issue the delete call, and subsequently load the updated shopping list
+                attrs.onClickFunction = {
+                    scope.launch {
+                        deleteShoppingListItem(item)
+                        setShoppingList(getShoppingList())
+                    }
+                }
+                // Display item priority and description
                 +"[${item.priority}] ${item.desc} "
             }
         }
     }
+
+    child(
+        // New Item text box
+        functionalComponent = InputComponent,
+        props = jsObject {
+            onSubmit = { input ->
+                // Replace number of ! into priority value submitted to the backend
+                val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
+                scope.launch {
+                    addShoppingListItem(cartItem)
+                    setShoppingList(getShoppingList())
+                }
+            }
+        }
+    )
 }
